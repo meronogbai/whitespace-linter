b46ab4e7b342bbcea7be04ea8c23f7080816fca9
Meron Ogbai <okbaymeron@gmail.com>
2020-10-04 21:56:39 +0300

    Add detector spec and test cases

---
+++ spec/detector_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../lib/detector'
+
+describe Detector do
+  test_file = Detector.new('test_file')
+  describe '#detect_whitespace' do
+    it 'detects trailing whitespace' do
+      expect(test_file.detect_whitespace).to eql({ 1 => "print 'Hello world'      \n", 4 => "print 'Good bye'  " })
+    end
+    it "doesn't detect empty lines" do
+      expect(test_file.detect_whitespace).not_to eql({ 3 => "\n", 4 => "\n" })
+    end
+  end
+  describe '#detect_empty' do
+    it 'detects extra empty lines' do
+      expect(test_file.detect_empty).to eql({ 3 => "\n" })
+    end
+    it "doesn't detect non empty lines" do
+      expect(test_file.detect_empty).not_to eql({ 1 => "print 'Hello world'      \n", 4 => "print 'Good bye'  " })
+    end
+  end
+end
