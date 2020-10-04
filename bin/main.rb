850279de64cc490eb39a8bcc9df71e0e473536b1
Meron Ogbai <okbaymeron@gmail.com>
2020-10-04 18:43:45 +0300

    Fix white space remover

--- bin/main.rb
+++ bin/main.rb
@@ -1,29 +1,33 @@
 #! /usr/bin/env ruby
 
-require_relative '../lib/detector.rb'
+require_relative '../lib/detector'
+require_relative '../lib/format'
 
-file_lint = Detector.new(ARGV[0])
+# file_lint = Detector.new(ARGV[0])
 
-extra_whitespace = file_lint.detect_whitespace
-unless extra_whitespace.empty?
-  puts
-  puts '....................................................'
-  puts
-  puts "The following lines have extra trailing whitespace."
-  puts
-  extra_whitespace.each do |num, line|
-    print "#{num} #{line}"
-  end
-end
+# extra_whitespace = file_lint.detect_whitespace
+# unless extra_whitespace.empty?
+#   puts
+#   puts '....................................................'
+#   puts
+#   puts "The following lines have extra trailing whitespace."
+#   puts
+#   extra_whitespace.each do |num, line|
+#     print "#{num} #{line}"
+#   end
+# end
 
-extra_empty_lines = file_lint.detect_empty
-unless extra_empty_lines.empty?
-  puts
-  puts '....................................................'
-  puts
-  puts "The following empty lines are duplicate."
-  puts
-  extra_empty_lines.each do |num, line|
-    print "#{num} #{line}"
-  end
-end
+# extra_empty_lines = file_lint.detect_empty
+# unless extra_empty_lines.empty?
+#   puts
+#   puts '....................................................'
+#   puts
+#   puts "The following empty lines are duplicate."
+#   puts
+#   extra_empty_lines.each do |num, line|
+#     print "#{num} #{line}"
+#   end
+# end
+
+file_format = Format.new(ARGV[0])
+file_format.format
--- lib/format.rb
+++ lib/format.rb
@@ -1,29 +1,45 @@
 class Format
   def initialize(file)
-    @file = File.open(file)
+    @file = file
     @temp = []
     temp_file
+    p @temp
   end
+
   private
+
   def temp_file
-    @file.each do |line|
+    @read_file = File.open(@file, 'r')
+    @read_file.each do |line|
       @temp << line
     end
   end
+
   def whitespace_remover
-    @temp.each do |line|
-      line.gsub(/\s+\n/, "\n")
+    @temp.each_with_index do |line, index|
+      if line.end_with?("\n")
+        @temp[index] = line.gsub(/\s+\n$/, "\n")
+      else
+        @temp[index] = line.gsub(/\s+$/, "")
+      end
     end
   end
+
   def empty_line_remover
     result = []
     @temp.each_with_index do |line, index|
-      @result << line unless @temp[index-1] == "\n" && line == "\n"
+      @result << line unless @temp[index - 1] == "\n" && line == "\n"
     end
     result
   end
-  public
-  def format
 
+  public
+
+  def format
+    # @new_file = File.open(@file, 'w')
+    whitespace_remover
+    p @temp
+    # empty_line_remover
+    # @new_file.write(@temp.join)
+  end
 end
-        
