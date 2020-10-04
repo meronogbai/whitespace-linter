9b958693a61cb67b761f06fe782a5a589e41777f
Meron Ogbai <okbaymeron@gmail.com>
2020-10-04 17:07:20 +0300

    Add detecting functionality to main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,29 @@
+#! /usr/bin/env ruby
+
 require_relative '../lib/detector.rb'
+
+file_lint = Detector.new(ARGV[0])
+
+extra_whitespace = file_lint.detect_whitespace
+unless extra_whitespace.empty?
+  puts
+  puts '....................................................'
+  puts
+  puts "The following lines have extra trailing whitespace."
+  puts
+  extra_whitespace.each do |num, line|
+    print "#{num} #{line}"
+  end
+end
+
+extra_empty_lines = file_lint.detect_empty
+unless extra_empty_lines.empty?
+  puts
+  puts '....................................................'
+  puts
+  puts "The following empty lines are duplicate."
+  puts
+  extra_empty_lines.each do |num, line|
+    print "#{num} #{line}"
+  end
+end
